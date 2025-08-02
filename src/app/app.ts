fd189e9b1432c80c0bee31815921b7520fafdfaa
yoavh28 <yoavh28@hotmail.com>
2025-08-02 15:10:16 +0300

    add heroes component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('Testing');
---
+++ src/app/heroes/heroes.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes.html',
+  styleUrls: ['./heroes.css']
+})
+export class HeroesComponent  {
+
+}
