65828e40222130d4302276286cd24e081408809d
yoavh28 <yoavh28@hotmail.com>
2025-08-03 13:19:51 +0300

    part 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroesComponent],
+  imports: [RouterOutlet, HeroesComponent, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('Testing');
+export class AppComponent  {
+  protected readonly title = signal('Tour of Heroes');
 }
--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.html',
   styleUrls: ['./heroes.css']
 })
 export class HeroesComponent  {
+  @Input() title!: string;
+  heroes = HEROES;
+  selectedHero?: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
